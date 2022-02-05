b8b5aee6c08e0dcbc0efd3bd16171127938e060d
Benoit Gloria <gloria_b@epita.fr>
2022-02-05 14:52:06 +0100

    add post hook at the end of backup

--- backup.sh
+++ backup.sh
@@ -1,6 +1,8 @@
 #!/bin/sh -e
 
 echo "do a backup"
+start_time=$(date +%s%3N)
+
 restic snapshots
 have_repo=$?
 
@@ -16,4 +18,10 @@ restic backup /tmp/nextcloud_pgsql.bak
 
 restic forget --keep-daily 7 --keep-weekly 4
 
+end_time=$(date +%s%3N)
+elapsed=$(( end_time - start_time ))
+if [ -n "${POST_HOOK_URL:-}" ]; then
+  url=$(echo "$POST_HOOK_URL" | sed s/\$\{elapsed\}/${elapsed}/)
+  curl "$url"
+fi
 echo "backup done"
